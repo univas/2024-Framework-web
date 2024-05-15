2552c6039be4199427ccea1d9c67a2a169c1152e
= <=>
2024-05-15 13:05:12 -0300

    aula 15-05 / add axios

--- usuarios-github/src/App.jsx
+++ usuarios-github/src/App.jsx
@@ -1,38 +1,68 @@
 import { useState } from 'react'
 import estilo from './app.module.css'
+import axios from 'axios'
 
 function App() {
   const [usuarioSelecionado, setUsuarioSelecionado] = useState(null)
+  const [erro, setErro] = useState()
+  const [usuarios, setUsuarios] = useState([])
+  const [repositorios, setRepositorios] = useState([])
 
-  const usuarios = [
-    {
-      usuario: "usuário 01",
-      url: "https://www.google.com"
-    },
-    {
-      usuario: "usuário 02",
-      url: "https://www.univas.edu.br"
-    }
-  ]
+  axios.get("https://api.github.com/users")
+    .then((res) => {
+      setUsuarios(res.data)
+    })
 
   function mudaUsuario(usuarioBuscado){
-    const usuarioFiltrado = usuarios.filter(usuario => usuario.usuario == usuarioBuscado)
-    setUsuarioSelecionado(usuarioFiltrado[0])
+    const usuarioFiltrado = usuarios.filter(usuario => usuario.login == usuarioBuscado)
+    if(usuarioFiltrado.length == 0){
+      setErro(`Usuário '${usuarioBuscado}' não encontrado`)
+    }else{
+      setErro(null)
+      setUsuarioSelecionado(usuarioFiltrado[0])
+    }
+  }
+
+  function voltar(){
+    setUsuarioSelecionado(null)
+    setRepositorios([])
+  }
+
+  function repos(event){
+    event.preventDefault()
+
+    if(repositorios.length > 0){
+      setRepositorios([])
+    }else if(event.target.href){
+      axios.get(event.target.href)
+        .then((res) => {
+          setRepositorios(res.data)
+        })
+    }
   }
 
   return (
     <div className='container mt-4'>
+      {
+        erro && (
+          <p className='alert alert-danger'>{erro}</p>
+        )
+      }
       {
         // Se usuarioSelecionado não estiver preenchido
         // Mostra o conteúdo em tela
         !usuarioSelecionado && (
           <ul className='list-group'>
             {/* Ctrl + Shift + Alt + (setinha) */}
-            <li onClick={() => mudaUsuario("usuário 01")} className={'list-group-item ' + estilo.cursor }>Usuário 01</li>
-            <li onClick={() => mudaUsuario("usuário 02")} className={'list-group-item ' + estilo.cursor }>Usuário 02</li>
-            <li onClick={() => mudaUsuario("usuário 03")} className={'list-group-item ' + estilo.cursor }>Usuário 03</li>
-            <li onClick={() => mudaUsuario("usuário 04")} className={'list-group-item ' + estilo.cursor }>Usuário 04</li>
-            <li onClick={() => mudaUsuario("usuário 05")} className={'list-group-item ' + estilo.cursor }>Usuário 05</li>
+            {
+              usuarios.map(usuario => {
+                return (
+                  <li key={usuario.login} onClick={() => mudaUsuario(usuario.login)} className={'list-group-item ' + estilo.cursor}>
+                    {usuario.login}
+                  </li>
+                )
+              })
+            }
           </ul>
         )
       }
@@ -40,8 +70,34 @@ function App() {
       {
         usuarioSelecionado && (
           <>
-            <h2>{ usuarioSelecionado.usuario }</h2>
-            <p><strong>URL:</strong> { usuarioSelecionado.url }</p>
+            <h2>
+              <img src={usuarioSelecionado.avatar_url} className={'img-fluid rounded-pill ' + estilo.avatar} />
+              { usuarioSelecionado.login } 
+              <span onClick={voltar} className={'mx-3 fs-6 btn btn-secondary btn-sm'}>Voltar</span>
+            </h2>
+            <p><strong>URLs:</strong></p>
+            <ul>
+              <li>
+                <a target='_blank' href={usuarioSelecionado.html_url}>Perfil</a>
+              </li>
+              <li>
+                <a onClick={repos} target="_blank" href={usuarioSelecionado.repos_url}>Repositórios</a>
+              </li>
+              <li>
+                <a target="_blank" href={usuarioSelecionado.followers_url}>Seguidores</a>
+              </li>
+
+            </ul>
+
+            {
+              repositorios.length > 0 && (
+                repositorios.map(repo => {
+                  return (
+                    <p key={repo.name}>{repo.name}</p>
+                  )
+                })
+              )
+            }
           </>
         )
       }
